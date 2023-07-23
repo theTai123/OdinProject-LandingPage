0732247cc6c5a15eb3a61915834d45b341254ecb
theTai123 <lamgiatai381@gmail.com>
2023-07-23 16:30:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var btn = document.querySelectorAll('button');
+
+btn.forEach(element => {
+    element.addEventListener('click',()=>{
+        window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley";
+    });
+});
